140e54dad9a8c02989faf42b5a95976addcd7b4d
obickford <bickfoor@miamioh.edu>
2022-05-08 14:25:56 -0400

    code

--- final for r.R
+++ final for r.R
@@ -10,10 +10,10 @@ updated_cenus_data <- census_data %>%
 
 updated_statedF <- merge(state_mapping_data, unemployment_data) ## merge two data sets with state name
 
-merged_data <- merge(updated_statedF, updated_cenus_data,
+merged_regions_data <- merge(updated_statedF, updated_cenus_data,
                      by.x= 'Code', by.y= 'State') ## take combined data and merge with state code
 
-cenus_regions_data <- merged_data %>% 
+cenus_regions_data <- merged_regions_data %>% 
   group_by(State) %>% # group by state first to find difference in rates 
   mutate(difference_in_rates = March_21 - March_22) %>% #set up difference rate variable 
   group_by(Region) %>%  # after the new variable group by the region to...
